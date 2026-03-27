cd75112d269e3537466ba87448ffae493d8fceff
Marina Yastrebova <yasstreb@gmail.com>
2026-03-27 23:30:05 +0200

    add refreshSession to controller and router

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+} from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
@@ -8,7 +12,8 @@ import {
 
 const router = Router();
 
-router.post('/register', celebrate(registerUserSchema), registerUser);
-router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
