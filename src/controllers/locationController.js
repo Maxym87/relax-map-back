61db04770dacb37bcae045e3b7e9c362a2f24118
nadiia-mishenina <ya.nadegda2014@gmail.com>
2026-03-27 23:55:31 +0200

    add update location endpoint with author check

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -26,3 +26,52 @@ export const createLocationController = async (req, res) => {
     });
   }
 };
+
+export const updateLocationController = async (req, res) => {
+  try {
+    const { locationId } = req.params;
+    const userId = req.user._id;
+
+    const location = await Location.findById(locationId);
+
+    if (!location) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Location not found',
+      });
+    }
+
+    if (location.author.toString() !== userId.toString()) {
+      return res.status(403).json({
+        status: 403,
+        message: 'Forbidden: only author can edit this location',
+      });
+    }
+
+    const updatedData = {
+      ...req.body,
+    };
+
+    if (req.file) {
+      updatedData.image = req.file.buffer.toString('base64');
+    }
+
+    const updatedLocation = await Location.findByIdAndUpdate(
+      locationId,
+      updatedData,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    return res.status(200).json({
+      status: 200,
+      data: updatedLocation,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
