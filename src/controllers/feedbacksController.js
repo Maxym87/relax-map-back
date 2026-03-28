ddf0ae244f36eddcb3d57f047591d25c5049a7dd
Evgeniya Kosykh <evgeniyapylypenko92@gmail.com>
2026-03-28 20:42:10 +0200

    add feedback backend functionality

---
+++ src/controllers/feedbacksController.js
@@ -0,0 +1,61 @@
+import createHttpError from 'http-errors';
+import * as feedbackService from '../services/feedbackService.js';
+import locationsService from '../services/locationsService.js';
+import { FEEDBACK_PAGINATION } from '../constants/pagination.js';
+import { getPagination } from '../helpers/pagination.js';
+
+const normalizeFeedbackIdRefs = (feedbacksId) =>
+  (feedbacksId || []).map((ref) => (ref && ref._id ? ref._id : ref));
+
+export const getLocationFeedbacks = async (req, res) => {
+  const { locationId } = req.params;
+  const { page, perPage, skip, limit } = getPagination(
+    req.query,
+    FEEDBACK_PAGINATION,
+  );
+
+  const location = await locationsService.getLocationById(locationId);
+
+  if (!location) {
+    throw createHttpError(404, 'Location not found');
+  }
+
+  const feedbacksIdRefs = normalizeFeedbackIdRefs(location.feedbacksId);
+  const filter = { _id: { $in: feedbacksIdRefs } };
+
+  const [feedbacks, totalFeedbacks] = await Promise.all([
+    feedbackService.findFeedbacks({ filter, skip, limit }),
+    feedbackService.countFeedbacks({ filter }),
+  ]);
+
+  const totalPages = Math.ceil(totalFeedbacks / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalFeedbacks,
+    totalPages,
+    feedbacks,
+  });
+};
+
+export const createFeedback = async (req, res) => {
+  const { locationId } = req.params;
+
+  const location = await locationsService.getLocationById(locationId);
+
+  if (!location) {
+    throw createHttpError(404, 'Location not found');
+  }
+
+  const payload = {
+    rate: req.body.rate,
+    description: req.body.description,
+    userName: req.user.name,
+    locationId,
+  };
+
+  const feedback = await feedbackService.createFeedback(payload);
+
+  res.status(201).json(feedback);
+};
--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,46 +1,42 @@
+mport { Feedback } from '../models/feedback.js';
 import { Location } from '../models/location.js';
 
-export const getLocations = async ({
-  page = 1,
-  limit = 10,
-  region,
-  type,
-  search,
-}) => {
-  const parsedPage = Number(page) || 1;
-  const parsedLimit = Number(limit) || 10;
-  const skip = (parsedPage - 1) * parsedLimit;
-
-  const filter = {};
-
-  if (region) filter.region = region;
-  if (type) filter.type = type;
-
-  if (search) {
-    filter.name = {
-      $regex: search,
-      $options: 'i',
-    };
-  }
-
-  const items = await Location.find(filter)
-    .populate('region')
-    .populate('type')
-    .skip(skip)
-    .limit(parsedLimit)
-    .sort({ createdAt: -1 });
-
-  const totalItems = await Location.countDocuments(filter);
-
-  const totalPages = Math.ceil(totalItems / parsedLimit);
-
-  return {
-    items,
-    page: parsedPage,
-    totalPages,
-  };
+const getAllLocations = (filter, sort, skip, limit) => {
+  });
 };
 
-export const getLocationById = async (id) => {
-  return await Location.findById(id).populate('region').populate('type');
+const pushFeedbackId = (locationId, feedbackId) => {
+  return Location.findByIdAndUpdate(locationId, {
+    $push: { feedbacksId: feedbackId },
+  });
+};
+
+const updateLocationAverageRate = async (locationId) => {
+  const location = await Location.findById(locationId).select('feedbacksId');
+  if (!location) {
+    return;
+  }
+  const ids = location.feedbacksId || [];
+  if (ids.length === 0) {
+    return;
+  }
+  const [row] = await Feedback.aggregate([
+    { $match: { _id: { $in: ids } } },
+    { $group: { _id: null, avgRate: { $avg: '$rate' } } },
+  ]);
+  if (row == null || row.avgRate == null) {
+    return;
+  }
+  const rounded = Math.round(row.avgRate * 2) / 2;
+  await Location.findByIdAndUpdate(locationId, { rate: rounded });
+};
+
+export default {
+  getAllLocations,
+  getAllLocationsCount,
+  getLocationById,
+  createLocation,
+  updateLocation,
+  pushFeedbackId,
+  updateLocationAverageRate,
 };
