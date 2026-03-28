888bc3346b1ada073e18aca6131f9898ac2dd707
Maks Shkafar <m.shkafar@gmail.com>
2026-03-28 22:05:23 +0100

    fix one part

--- src/services/feedbackService.js
+++ src/services/feedbackService.js
@@ -1,26 +1,26 @@
-import { Feedback } from '../models/feedback.js';
-import { FEEDBACK_SORT_BY_CREATED_AT } from '../constants/feedback.js';
-import locationsService from './locationsService.js';
+// import { Feedback } from '../models/feedback.js';
+// import { FEEDBACK_SORT_BY_CREATED_AT } from '../constants/feedback.js';
+// import locationsService from './locationsService.js';
 
-export const findFeedbacks = ({ filter, skip, limit }) => {
-  return Feedback.find(filter)
-    .sort(FEEDBACK_SORT_BY_CREATED_AT)
-    .skip(skip)
-    .limit(limit);
-};
+// export const findFeedbacks = ({ filter, skip, limit }) => {
+//   return Feedback.find(filter)
+//     .sort(FEEDBACK_SORT_BY_CREATED_AT)
+//     .skip(skip)
+//     .limit(limit);
+// };
 
-export const countFeedbacks = ({ filter }) => {
-  return Feedback.countDocuments(filter);
-};
+// export const countFeedbacks = ({ filter }) => {
+//   return Feedback.countDocuments(filter);
+// };
 
-export const createFeedback = async (payload) => {
-  const { locationId, rate, description, userName } = payload;
-  const feedback = await Feedback.create({
-    rate,
-    description,
-    userName,
-  });
-  await locationsService.pushFeedbackId(locationId, feedback._id);
-  await locationsService.updateLocationAverageRate(locationId);
-  return Feedback.findById(feedback._id);
-};
+// export const createFeedback = async (payload) => {
+//   const { locationId, rate, description, userName } = payload;
+//   const feedback = await Feedback.create({
+//     rate,
+//     description,
+//     userName,
+//   });
+//   await locationsService.pushFeedbackId(locationId, feedback._id);
+//   await locationsService.updateLocationAverageRate(locationId);
+//   return Feedback.findById(feedback._id);
+// };
